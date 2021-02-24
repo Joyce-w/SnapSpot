b84fd1c735d371c33a049528b2890c67ec911587
Joyce <hellojoycewong@gmail.com>
2021-02-23 21:51:32 -0800

    coord

--- static/find_coord.js
+++ static/find_coord.js
@@ -6,6 +6,7 @@ let lat;
 mapboxgl.accessToken =
     'pk.eyJ1Ijoiam95am95am95eSIsImEiOiJja2w4YzZyM3kxcTdmMnZwZXdiNG5yczRjIn0.CDJtfCb3X8TKcTBRMPBJFA';
 var coordinates = document.getElementById('coordinates');
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -13,17 +14,22 @@ var map = new mapboxgl.Map({
     zoom: 2
 });
 
+//make popup variable with button embedded
+let popup = new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<button id="btn">Share location</button')
+
+//create blue marker and add to the map
 var marker = new mapboxgl.Marker({
         draggable: true
     })
     .setLngLat([0, 0])
-    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-    .setHTML('<button id="btn"><a>Share location</a></button'))
+    .setPopup(popup)
     .addTo(map);
 
+//displays coords when marker is finishes moving
 marker.on('dragend', onDragEnd);
 
-
+//display coordinates depending on marker location on canvas
 function onDragEnd() { 
     var lngLat = marker.getLngLat();
     coordinates.style.display = 'block';
@@ -35,20 +41,8 @@ function onDragEnd() {
 
 }
 
-// make sure DOM is ready 
-document.ready(function(){
-    button.on('click', function (e) {
-        alert("you clicked a button")
-    })
-    // Other event handlers.
-});
-
-//--- also tried this and it doesnt work-----
-    // even listener for click button in marker
-button.click(function(e) {
+// even listener for click button in marker
+// does not work
+$('#btn').click(function(e) {
     alert("you clicked")
-    //send axios data to python
-    
-    
-    
 })
